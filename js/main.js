b531b2218b354c4eb6047659a235b0e2992a39ff
jiyxxn <jiyunk@naver.com>
2023-08-09 17:50:21 +0900

    커밋 완료!

--- js/main.js
+++ js/main.js
@@ -6,14 +6,35 @@ window.addEventListener("DOMContentLoaded", function(){
   }, 4000);
 })
 
-// 이미지 프리로드
+/** 새로고침하면 맨 위로 */
+window.onload = function(){
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  })
+}
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+/** 이미지 프리로드 */
 function preloading(imageArray){
   for(let i=0; i <imageArray.length; i++){
     let img = new Image();
     img.src = imageArray[i];
   }
 }
-
 preloading([
   'images/cloud_panorama1-min.png',
   'images/main_bg-min.png',
@@ -28,16 +49,16 @@ preloading([
   'images/Mockup_oth-min.png'
 ])
 
-// 메인
 
-const main = document.querySelector(".main");
+// ❗❗ 메인
+
+const main = document.querySelector("section.main");
 const mainText = document.querySelector(".mainText");
-
+/** De: 뒤에 붙는 텍스트 */
 const $text = document.querySelector(".others");
-
 const subTexts = document.querySelector("div.subText");
-const des = document.querySelector("p.des");
-const dev = document.querySelector("p.dev");
+/** 괄호 속 서브텍스트 */
+const subTextsElements = document.querySelector("div.subText > div");
 
 const body = document.querySelector("body");
 const navIcon = document.getElementById("navIcon");
@@ -51,26 +72,26 @@ let reload = setTimeout(()=>{
 }, 5000);
 
 let reload2 = setTimeout(()=>{
-  mainText.classList.add("on"); // 타이포할 텍스트 opacity 1;
-
+  mainText.classList.add("on");
+  /** De:corative, De:signer 텍스트 뜨기 */
   setTimeout(function(){
-    typo(letters); //decorative, designer 텍스트 뜨고
+    typo(letters);
   },500)
 
-  let sub = setTimeout(()=>{  // "감각있는 디자이너 텍스트"
+  /** "감각있는 디자이너 텍스트" */
+  setTimeout(function(){
     subTexts.classList.add("on");
-    des.classList.add("on");
-  }, 7000)
+    subTextsElements.children[0].classList.add("on");
+  }, 7000);
 
-
-
+  /** Developer 컨셉 변경 */
   let typingText2 = setTimeout(()=>{
 
     subTexts.classList.remove("on");
-    des.classList.remove("on");
+    subTextsElements.children[0].classList.remove("on");
     // 밤하늘 이미지 시작
     main.classList.add("on");
-    // designer 지우기
+    /** Designer 텍스트 지우기 */
     setTimeout(function(){
       for(i=0; i<letters.length; i++){
   
@@ -90,7 +111,7 @@ let reload2 = setTimeout(()=>{
 
     let sub = setTimeout(()=>{
       subTexts.classList.add("on");
-      dev.classList.add("on");
+      subTextsElements.children[1].classList.add("on");
     }, 7000);
 
     let scrollTo = setTimeout(()=>{
@@ -103,16 +124,15 @@ let reload2 = setTimeout(()=>{
   }, 9000);
   
 
-}, 7500); // 상단 텍스트 뜰 때까지 3초 딜레이
+}, 7500); // De: 전 텍스트들 뜰 때까지 딜레이
 
 
-
-  //글자 모음
+  /** Decorative, Designer 배열 */
   const letters = [
     "corative",
     "signer",
   ];
-
+  /** Deep, Developer 배열 */
   const letters2 = [
     "ep",
     "veloper"
@@ -120,13 +140,12 @@ let reload2 = setTimeout(()=>{
 
 
 function typo(words){
-  // const $text = document.querySelector(".others");
 
-  // 글자 입력 속도
+  /** 글자 입력 속도 */
   const speed = 100;
   let i = 0;
   
-  //타이핑 효과 (wait 기능을 위한 async, await)
+  /** 타이핑 효과 (wait 기능을 위한 async, await) */
   const typing = async () => {
     const letter = words[i].split("");
   
@@ -144,7 +163,7 @@ function typo(words){
     
   }
   
-  // 글자 지우는 효과
+  /** 글자 지우는 효과 */
   const remove = async ()=> {
     const letter = words[i].split("");
   
@@ -160,7 +179,7 @@ function typo(words){
     typing();
   }
   
-  // 딜레이 기능 (마이크로 초)
+  /** 딜레이 기능(마이크로초) */
   function wait(ms) {
     return new Promise(res => setTimeout(res, ms))
   }
@@ -169,9 +188,8 @@ function typo(words){
   setTimeout(typing, 1000);
 }
 
-// 커서
+// ❗❗ 커서
 var cursor = document.querySelector('.cursor');
-var cursorinner = document.querySelector('.cursor2');
 var a = document.querySelectorAll('a');
 
 document.addEventListener('mousemove', function(e){
@@ -180,12 +198,11 @@ document.addEventListener('mousemove', function(e){
   cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
 });
 
-document.addEventListener('mousedown', function(){
-  cursor.classList.add('cursorinnerhover')
-});
-
 document.addEventListener('mouseup', function(){
   cursor.classList.remove('cursorinnerhover')
+});
+document.addEventListener('mousedown', function(){
+  cursor.classList.add('cursorinnerhover')
 });
 
 a.forEach(item => {
@@ -198,16 +215,8 @@ a.forEach(item => {
 })
 
 
-document.addEventListener('mousedown', function(){
-  cursor.classList.add('click');
-});
 
-document.addEventListener('mouseup', function(){
-  cursor.classList.remove('click')
-});
-
-
-// 햄버거
+// ❗❗ 햄버거
 
 const bg = document.querySelector("div.bg");
 
@@ -223,7 +232,20 @@ navIcon.addEventListener("click", e => {
   }
 })
 
-// 디자인
+// ❗❗ DesignWorks Section
+const designItem = document.querySelectorAll(".designWorks > ul.pcVer > li");
+
+for(let i=0; i<designItem.length; i++){
+  designItem[i].addEventListener("click", e=> {
+
+    designItem[i].classList.remove("on");
+    // 애니메이션 반복동작
+    e.target.offsetWidth;
+    designItem[i].classList.add("on");
+  })
+}
+
+// ❗❗ 디자인 가로스크롤
 let design = document.querySelector("section.designWorks");
 let designTop = design.offsetTop;
 let designBox = document.querySelector("section.designWorks > ul:first-of-type")
@@ -239,33 +261,41 @@ if (matrixType === '2d'){
 }
 
 window.addEventListener("scroll", e=> {
+  let y = window.scrollY;
+
   window.addEventListener("resize", e => {
     designTop = design.offsetTop;
   })
-  let y = window.scrollY;
 
   if(y > designTop){
     design.classList.add("on");
-    designBox.style.transform = `translateX(${x - y/3}px)`
+    designBox.style.transform = `translateX(${1.95*x - y/2.5}px)`
   } else {
     design.classList.remove("on");
   }
 })
 
-// const designItem = document.querySelectorAll(".designWorks > ul:first-of-type > li");
-// console.log(designItem);
-// for(let i=0; i<designItem; i++){
-//   designItem[i].addEventListener("click", e=> {
-//     designItem[i].classList.add("on");
-//   })
-//   console.log(i)
-// }
 
-// 웹 프로젝트
+
+// ❗❗ 웹 프로젝트
+let web = document.querySelector("section.web");
+let webTop = web.offsetTop;
+let webCircle = document.querySelector("section.web > div.webCircle")
+
+window.addEventListener("scroll", e=> {
+  let y = window.scrollY;
+
+  window.addEventListener("resize", e => {
+    webTop = web.offsetTop;
+  })
+
+  if(y >= webTop/1.2){
+    webCircle.style.transform = `scale(${0.5 + y/4000})`
+  }
+})
+
 const lis = document.querySelectorAll("section.web > ul > li > a");
 const arts = document.querySelectorAll("section.web > article");
-console.log(lis, arts)
-
 
 for(let i=0; i<lis.length; i++){
 
@@ -276,6 +306,26 @@ for(let i=0; i<lis.length; i++){
     })
     arts[i].classList.add("on");
 
+
+    const className = arts[i].className;
+    console.log(className);
+    switch(className){
+      case 'webPage bing on':
+        webCircle.style.background = 'linear-gradient(180deg, #F4FCFF 0%, #E4F7FF 8%, #FFF 80%)';
+        break;
+      case 'webPage danggn on':
+        webCircle.style.background = 'linear-gradient(180deg, #ffd5ba 0%, #FFE2CE 8%, #FFF 80%)';
+        break;
+      case 'webPage oth on':
+        webCircle.style.background = 'linear-gradient(180deg, #345543 0%, #C0C9B5 8%, #FFF 80%)';
+        break;
+      case 'mobile on':
+        webCircle.style.background = 'pink';
+        break;
+      default :
+        webCircle.style.background = '#fff'
+    }
+
   })
   lis[i].addEventListener("mouseout", e=> {
     arts.forEach(item => {
@@ -284,18 +334,9 @@ for(let i=0; i<lis.length; i++){
     lis.forEach(item => {
       item.parentElement.classList.add("on");
     })
+    webCircle.style.background = '#fff'
   })
 }
-
-// 디자인 스크롤
-
-
-
-
-
-
-
-
 // 모바일 * 프로필탭
 const profile = document.querySelector(".profile");
 const moblis = document.querySelectorAll("ul.moblist > li > a");
